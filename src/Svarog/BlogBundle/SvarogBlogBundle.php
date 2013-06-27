518567af95236f05800ab638e988f058080502d0
Sergey Vetko <svaroggg@gmail.com>
2013-06-27 18:26:20 +0400

    Initial version with symfony 2.3.1

---
+++ src/Svarog/BlogBundle/SvarogBlogBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Svarog\BlogBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class SvarogBlogBundle extends Bundle
+{
+}
---
+++ src/Svarog/PanelBundle/SvarogPanelBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Svarog\PanelBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class SvarogPanelBundle extends Bundle
+{
+}
